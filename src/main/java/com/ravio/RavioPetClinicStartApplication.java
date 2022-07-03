2a3204a1a9a1d3bef58c2fc382be773a59f68d0a
Ravindra Rauthan <RAUTHANR@amdocs.com>
2022-07-03 13:16:59 +0530

    First commit

---
+++ src/main/java/com/ravio/RavioPetClinicStartApplication.java
@@ -0,0 +1,13 @@
+package com.ravio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RavioPetClinicStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RavioPetClinicStartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravio/RavioPetClinicStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RavioPetClinicStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
